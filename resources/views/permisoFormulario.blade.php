e9aae8307920deee37107809bc0e80178f5c523e
matias <matias.ducid08@gmail.com>
2020-06-26 19:18:45 -0300

    limpieza del form. Seteo de atributo aprobado en false

--- resources/views/permisoFormulario.blade.php
+++ resources/views/permisoFormulario.blade.php
@@ -68,9 +68,6 @@
                 <label for="espacio">Espacio Especifico:</label>
                 <input type="text" class="form-control" name="espacio" value="{{old('espacio')}}"placeholder="¿En que espacio especifico trabaja?">
 
-                <input type="text" class="form-control" name="aprobado" value="False" placeholder="False" style="display:none" >
- 
-
                 <br>
                 <button type="submit" class="btn btn-secondary">Guardar</button>
             </div>
