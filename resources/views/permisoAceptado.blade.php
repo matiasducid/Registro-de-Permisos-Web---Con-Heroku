22d5b19054b451789a38208cf33b3bf9da8f536a
LuluAllende19 <abigail_lucii@hotmail.com>
2020-07-03 16:13:10 -0300

    Tratando de enviar mail de acpetacion

--- resources/views/permisoAceptado.blade.php
+++ resources/views/permisoAceptado.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('static/css/app.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <title>Permiso Aceptado</title>
 </head>
 <body>
