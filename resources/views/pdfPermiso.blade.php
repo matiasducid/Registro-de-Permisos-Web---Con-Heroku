1d7f85711948efebe199d862590bca67f68814f7
LuluAllende19 <abigail_lucii@hotmail.com>
2020-07-04 17:05:18 -0300

    Imprimir en pdf

--- resources/views/pdfPermiso.blade.php
+++ resources/views/pdfPermiso.blade.php
@@ -20,45 +20,46 @@
 <form action="" method="get">
     @csrf
         <div class="form-group text-center">
-
+            <div>
             <label class="col-sm-4"for="dni">DNI:
                 <input type="text" class="form-control" name="dni" value="{{$permiso[0]->dni}}" disabled>
             </label>
-            
-
+            </div>
+            <div>
             <label class="col-sm-7" for="apellido">Apellido:
                 <input type="text" class="form-control" name="apellido" value="{{$permiso[0]->apellido}}" disabled>
             </label>
-            
-
-            <label class="col-sm-4" for="nombre">Nombre:
+            </div>
+            <div>
+            <label class="col-sm-6" for="nombre">Nombre:
                 <input type="text" class="form-control" name="nombre" value="{{$permiso[0]->nombre}}" disabled>
             </label>
-            
-
-            <label class="col-sm-7" for="email">E-Mail:
+            </div>
+            <div>
+            <label class="col-sm-5" for="email">E-Mail:
                 <input type="text" class="form-control" name="email" value="{{$permiso[0]->email}}" disabled>
             </label>
-            
-
-            <label class="col-sm-4" for="sector">Sector:
+            </div>
+            <div>
+            <label class="col-sm-5" for="sector">Sector:
                 <input type="text" class="form-control" name="sector" value="{{$permiso[0]->sector}}" disabled>
             </label>
-            
-            
-            <label class="col-sm-7" for="superior">Superior:
+            </div>
+            <div>
+            <label class="col-sm-6" for="superior">Superior:
                 <input type="text" class="form-control" name="superior" value="{{$permiso[0]->superior}}" disabled>
             </label>
-            
-
+            </div>
+            <div>
             <label class="col-sm-11" for="dependencia">Dependencia:
                 <input type="text" class="form-control" name="dependencia" value="{{$permiso[0]->dependencia}}" disabled>
             </label>
-            
-
+            </div>
+            <div>
             <label class="col-sm-11" for="espacio">Espacio Especifico:
                 <input type="text" class="form-control" name="espacio" value="{{$permiso[0]->espacio}}" disabled>
             </label>
+            </div>
 
         </div>
 </form>   
