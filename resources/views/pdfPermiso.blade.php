9a0b1508924716e954bd00c28352cb151b2a8ee5
LuluAllende19 <abigail_lucii@hotmail.com>
2020-07-04 14:51:46 -0300

    Imprimir en pdf

--- resources/views/pdfPermiso.blade.php
+++ resources/views/pdfPermiso.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"  media="print"/>
-    <link href="{{ asset('static/css/app.css') }}" rel="stylesheet" type="text/css"  media="print"/>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('static/css/app.css') }}" rel="stylesheet" type="text/css"/>
 
     <title>Permiso</title>
 </head>
