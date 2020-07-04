b5fe4ca5acdaf8dea6897d63575ab820faebb2ad
LuluAllende19 <abigail_lucii@hotmail.com>
2020-07-04 14:41:57 -0300

    Imprimir en pdf

--- resources/views/pdfPermiso.blade.php
+++ resources/views/pdfPermiso.blade.php
@@ -17,57 +17,59 @@
         </li>
     </nav>
 
+
     <div class="container col-sm-7 tituloForm">
         <h1>Solicitud de Permiso</h1>
     </div>
 
     <div class="container col-sm-8 border formPermiso">
 
-        <form action="" method="get">
-            @csrf
-                <div class="form-group text-center">
+        <div class="form-group text-center">
+        
+            <label class="col-sm-4"for="dni">Numero de Solicitud: 
+                <p>{{$permiso[0]->id}}</p>
+            </label>
 
-                    <label class="col-sm-4"for="dni">DNI:
-                        <input type="text" class="form-control" name="dni" value="{{$permiso[0]->dni}}" disabled>
-                    </label>
-                    
+            <label class="col-sm-4"for="dni">DNI:
+                <p>{{$permiso[0]->dni}}}</p>
+            </label>
+            
 
-                    <label class="col-sm-7" for="apellido">Apellido:
-                        <input type="text" class="form-control" name="apellido" value="{{$permiso[0]->apellido}}" disabled>
-                    </label>
-                    
+            <label class="col-sm-7" for="apellido">Apellido:
+                <p>{{$permiso[0]->apellido}}</p>
+            </label>
+            
 
-                    <label class="col-sm-6" for="nombre">Nombre:
-                        <input type="text" class="form-control" name="nombre" value="{{$permiso[0]->nombre}}" disabled>
-                    </label>
-                    
+            <label class="col-sm-6" for="nombre">Nombre:
+                <p>{{$permiso[0]->nombre}}</p>
+            </label>
+            
 
-                    <label class="col-sm-5" for="email">E-Mail:
-                        <input type="text" class="form-control" name="email" value="{{$permiso[0]->email}}" disabled>
-                    </label>
-                    
+            <label class="col-sm-5" for="email">E-Mail:
+                <p>Numero de Solicitud: {{$permiso[0]->email}}</p>
+            </label>
+            
 
-                    <label class="col-sm-5" for="sector">Sector:
-                        <input type="text" class="form-control" name="sector" value="{{$permiso[0]->sector}}" disabled>
-                    </label>
-                    
-                    
-                    <label class="col-sm-6" for="superior">Superior:
-                        <input type="text" class="form-control" name="superior" value="{{$permiso[0]->superior}}" disabled>
-                    </label>
-                    
+            <label class="col-sm-5" for="sector">Sector:
+                <p>Numero de Solicitud: {{$permiso[0]->sector}}</p>
+            </label>
+            
+            
+            <label class="col-sm-6" for="superior">Superior:
+                <p>Numero de Solicitud: {{$permiso[0]->superior}}</p>
+            </label>
+            
 
-                    <label class="col-sm-11" for="dependencia">Dependencia:
-                        <input type="text" class="form-control" name="dependencia" value="{{$permiso[0]->dependencia}}" disabled>
-                    </label>
-                    
+            <label class="col-sm-11" for="dependencia">Dependencia:
+                <p>Numero de Solicitud: {{$permiso[0]->dependencia}}</p>
+            </label>
+            
 
-                    <label class="col-sm-11" for="espacio">Espacio Especifico:
-                        <input type="text" class="form-control" name="espacio" value="{{$permiso[0]->espacio}}" disabled>
-                    </label>
+            <label class="col-sm-11" for="espacio">Espacio Especifico:
+                <p>Numero de Solicitud: {{$permiso[0]->espacio}}</p>
+            </label>
 
-                </div>
-        </form>   
+        </div> 
     </div>
 
 </body>
