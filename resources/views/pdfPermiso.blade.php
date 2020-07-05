2979ec0b5cefbe6392b131b0f049fb8c57b9f115
LuluAllende19 <abigail_lucii@hotmail.com>
2020-07-05 11:37:37 -0300

    Codigo qr

--- resources/views/pdfPermiso.blade.php
+++ resources/views/pdfPermiso.blade.php
@@ -66,6 +66,10 @@
             </label>
             </div>
 
+            <div class="title m-b-md">
+            {!!QrCode::size(300)->generate("www.nigmacode.com") !!}
+            </div>
+
         </div>
 </form>   
 </div>
