e72a5b180cb2ed122e0288bf92f43cd4039703dc
LuluAllende19 <abigail_lucii@hotmail.com>
2020-07-06 12:31:43 -0300

    Mejorando vistas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,104 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-        <title>Sistema de Permisos</title>
+@section('content')
+    <!-- Contenido de la sección -->
+    <div class="title m-b-md">
+        Bienvenidos al Sistema de Solicitud de Permisos
+    </div>
+    
+    <div>       
+        <a href="{{url('solicitarpermiso')}}">
+            <button class="btn btn-secondary">Solicitar Permiso</button>
+        </a>              
+    </div>
+@endsection
 
-        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 34px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-
-        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-            <a class="nav-link" href="{{url('/')}}">Sistema de Solicitud de Permisos</a>
-            </li>
-        </ul>
-        </nav>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Bienvenidos al Sistema de Solicitud de Permisos
-                </div>
-                <div>
-                <a href="{{url('solicitarpermiso')}}">
-                    <button class="btn btn-secondary">Solicitar Permiso</button>
-                </a>              
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
