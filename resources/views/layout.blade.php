da6c73ee3672466735bce7bd17c1d7373908df3f
LuluAllende19 <abigail_lucii@hotmail.com>
2020-07-06 12:36:27 -0300

    Mejorando vistas

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Sistema de Permisos</title>
+        <title>@yield('title')</title>
 
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,14 @@
 @extends('layout')
 
+@section('title', Sistema de Permisos)
+
+   
 @section('content')
     <!-- Contenido de la sección -->
     <div class="title m-b-md">
         Bienvenidos al Sistema de Solicitud de Permisos
     </div>
-    
+
     <div>       
         <a href="{{url('solicitarpermiso')}}">
             <button class="btn btn-secondary">Solicitar Permiso</button>
