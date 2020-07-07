813ca6f2ac853d47a01b5853ba10dc51bc133366
LuluAllende19 <abigail_lucii@hotmail.com>
2020-07-06 21:09:45 -0300

    Tratando de correr imagen a la derecha

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -69,14 +69,17 @@
     <body>
 
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link align-right" href="{{url('/')}}"><p class="nombre_organizacion"></p></a>
-                </li>
-            </ul>
             <div class="container">
                 <div class="row">
-                    <div class="col-xs-12 pull-right">
+                    <div class="col-xs-6">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link align-right" href="{{url('/')}}"><p class="nombre_organizacion"></p></a>
+                        </li>
+                    </ul>
+                    </div>
+               
+                    <div class="col-xs-6 pull-right">
                         <img src="{{ asset('images/albatroz-unpsjb.png')}}" class="logo_superior_derecho pull-right">
                     </div>
                 </div>
